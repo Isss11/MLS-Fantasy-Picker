a46dbb4fc8ddf36adf84b106d694e415c884ea89
Isaiah Sinclair <isaiah.sinclair.0711@gmail.com>
2023-04-21 21:27:56 -0400

    Now can parse general data for each player.

--- dataImporter.py
+++ dataImporter.py
@@ -1,54 +1,111 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from getpass import getpass
 
-# Fantasy IDs correspond to the ID that the MLS Fantasy Website assigned to it
-playerFantasyIDs = []
-
-email = input("Enter your MLS Fantasy email: ")
-password = input("Enter your MLS Fantasy password: ")
-
-driver = webdriver.Firefox()
-driver.get("https://fantasy.mlssoccer.com/#login_gigya")
-
-# https://stackoverflow.com/questions/27112731/selenium-common-exceptions-nosuchelementexception-message-unable-to-locate-ele
-
-driver.maximize_window() # For maximizing window
-
-# tells the driver to wait for the page to load so we can properly extract elements
-driver.implicitly_wait(5) 
-
-# sending password and email and logging into MLS Fantasy
-emailInput = driver.find_element(By.ID, "gigya-textbox-141263563588013010")
-emailInput.send_keys(email)
-passwordInput = driver.find_element(By.CLASS_NAME, "gigya-input-password")
-passwordInput.send_keys(password)
-loginButton = driver.find_element(By.CLASS_NAME, "gigya-input-submit")
-loginButton.click()
-
-# now getting Stats Centre webpage and clicking load more (a maximum of) 20 times to make sure we're getting all the needed players
-driver.get("https://fantasy.mlssoccer.com/#stats-center")
-
-for i in range(20):
-    driver.implicitly_wait(1)
-    try:
-        loadMoreButton = driver.find_element(By.CLASS_NAME, "load-more")
-    # once we the load button disappears (no more players to load)
-    except Exception:
-        break  
+class MLSData:
+    def __init__(self) -> None:
+        self.fantasyPlayerIDs = []
         
-    loadMoreButton.click()
+    # extracts the MLS Fantasy website IDs of the 
+    def extractFantasyIDs(self, email, password) -> None:
+        self.driver = webdriver.Firefox()
+        self.driver.get("https://fantasy.mlssoccer.com/#login_gigya")
 
-# now getting all player IDs
-driver.implicitly_wait(5) 
+        # https://stackoverflow.com/questions/27112731/selenium-common-exceptions-nosuchelementexception-message-unable-to-locate-ele
 
-# only retrieves players currently on screen (have to traverse through page)
-playersRows = driver.find_elements(By.CLASS_NAME, "js-player-modal")
+        self.driver.maximize_window() # For maximizing window
 
-for i in playersRows:
-    playerFantasyIDs.append(int(i.get_attribute('data-player_id')))
+        # tells the driver to wait for the page to load so we can properly extract elements
+        self.driver.implicitly_wait(5) 
 
-"""
-Extracting all MLS Fantasy IDs, then will search MLS website for each players data (given fantasy ID), and then put it in a database.
+        # sending password and email and logging into MLS Fantasy
+        emailInput = self.driver.find_element(By.ID, "gigya-textbox-141263563588013010")
+        emailInput.send_keys(email)
+        passwordInput = self.driver.find_element(By.CLASS_NAME, "gigya-input-password")
+        passwordInput.send_keys(password)
+        loginButton = self.driver.find_element(By.CLASS_NAME, "gigya-input-submit")
+        loginButton.click()
 
-Might want to log out of website later.
-"""
+        # now getting Stats Centre webpage and clicking load more (a maximum of) 20 times to make sure we're getting all the needed players
+        self.driver.get("https://fantasy.mlssoccer.com/#stats-center")
+
+        for i in range(20):
+            self.driver.implicitly_wait(1)
+            try:
+                loadMoreButton = self.driver.find_element(By.CLASS_NAME, "load-more")
+            # once we the load button disappears (no more players to load)
+            except Exception:
+                break  
+                
+            loadMoreButton.click()
+
+        # now getting all player IDs
+        self.driver.implicitly_wait(5) 
+
+        # only retrieves players currently on screen (have to traverse through page)
+        playersRows = self.driver.find_elements(By.CLASS_NAME, "js-player-modal")
+
+        for i in playersRows:
+            self.fantasyPlayerIDs.append(int(i.get_attribute('data-player_id')))
+    # extracts player IDs from a given webpage and returns a dictionary
+    def extractPlayerFantasyData(self):
+        generalFantasyAttrDict = {}
+        
+        # getting data from tables
+        playerName = (self.driver.find_element(By.CLASS_NAME, "profile-name")).text
+        generalFantasyAttrDict.update({"player_name" : playerName})
+        
+        playerTeam = (self.driver.find_element(By.CLASS_NAME, "player-team")).text
+        generalFantasyAttrDict.update({"player_team" : playerTeam})
+        
+        playerPosPriceStringArray = ((self.driver.find_element(By.CLASS_NAME, "player-pos")).text).split(" | ")
+        
+        playerPos = playerPosPriceStringArray[0]
+        generalFantasyAttrDict.update({"player_pos" : playerPos})
+        
+        playerPrice = playerPosPriceStringArray[1]
+        playerPrice = float(playerPrice[2:len(playerPrice) - 1])
+        generalFantasyAttrDict.update({"player_price" : playerPrice})
+        
+        # getting if player is playing by asking for a needed class
+        try:
+            self.driver.find_element(By.CLASS_NAME, "playing")
+            playerAvail = True
+        except Exception:
+            playerAvail = False
+        
+        generalFantasyAttrDict.update({"player_avail" : str(playerAvail)})
+            
+        generalFantasyStatLabels = self.driver.find_elements(By.CLASS_NAME, "pl-stat-label")
+        generalFantasyStats = self.driver.find_elements(By.CLASS_NAME, "pl-stat-data")
+        
+        # replacing element references with their corresponding texts
+        for i in range(len(generalFantasyStatLabels)):
+            generalFantasyStatLabels[i] = generalFantasyStatLabels[i].text
+            generalFantasyStats[i] = generalFantasyStats[i].text
+            
+            generalFantasyStatLabels[i] = generalFantasyStatLabels[i].replace(" ", "_")
+            generalFantasyStatLabels[i] = generalFantasyStatLabels[i].lower()
+        
+        # updating dictionary with label names and corresponding values
+        for i in range(len(generalFantasyStatLabels)):
+            generalFantasyAttrDict.update({generalFantasyStatLabels[i] : generalFantasyStats[i]})
+            
+        return generalFantasyAttrDict
+            
+    # calls on needed functions to get all our MLS Fantasy data  
+    def extractMLSFantasyData(self, email, password) -> None:
+        self.extractFantasyIDs(email, password)
+        
+        for i in self.fantasyPlayerIDs:
+            fantasyData.driver.get("https://fantasy.mlssoccer.com/#stats-center/player-profile/" + str(i))
+            self.driver.implicitly_wait(1) 
+            print(self.extractPlayerFantasyData())
+        
+
+if __name__ == "__main__":
+    email = input("Enter your MLS Fantasy Account email: ")
+    password = getpass("Enter your MLS Fantasy Account password (won't display your input): ")
+    
+    fantasyData = MLSData()  
+    fantasyData.extractMLSFantasyData(email, password)
